70bc33790ee047e0dca82eb184616727d92668b5
Dalsaba <correialopesl@gmail.com>
2021-11-25 16:46:19 +0100

    update CV2

--- index.js
+++ index.js
@@ -74,4 +74,11 @@ function affichModal() {
     }
 }    
 
+var menuButton = document.querySelector('#menu-button');
+var menu = document.querySelector('#menu');
+// show or hide
+menuButton.addEventListener('click',function(){
+  menu.classList.toggle('show-menu');
+  menuButton.classList.toggle('close');
+});
 }
