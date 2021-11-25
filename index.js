1d8a27cc426f3cb34e0fab5ebc02fa2ebc67915d
Dalsaba <correialopesl@gmail.com>
2021-11-25 14:23:02 +0100

    update CV

--- index.js
+++ index.js
@@ -22,10 +22,56 @@ function myFunction() {
 function affichProjectbox () { 
     projet.forEach(function (element) {
         var div = document.createElement("button");
-        div.className = "projetBox";
+        div.className = "btn btn-outline-primary";
         div.innerHTML = element;
         div.setAttribute("onClick", "myFunction()");
         document.getElementById("section2-2").appendChild(div);
     });
+
+function suppr() {
+            var div = document.createElement("p");
+            document.getElementById("tata").innerHTML = "";
+            document.getElementById("tata").appendChild(div);
+        }
+
+        function replace() {
+            const entry = String(window.prompt("Quelles modifications?", ""));
+            var div = document.createElement("p");
+            document.getElementById("tata").innerHTML = entry;
+            document.getElementById("tata").appendChild(div);
+        }
+        function add() {
+            const entry = String(window.prompt("Quelles modifications?", ""));
+            var div = document.createElement("p");
+            div.innerHTML = entry; 
+            document.getElementById("tata").appendChild(div);
+        }    
+
+function affichModal() {
+    // Get the modal
+    var modal = document.getElementById("myModal");
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+    modal.style.display = "block";
+
+    }
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+    }
+}    
 
 }
