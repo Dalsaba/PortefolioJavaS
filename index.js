61f1c5c4942be2e84ba6f8a66409732b60173785
Dalsaba <correialopesl@gmail.com>
2021-11-26 09:23:41 +0100

    portefolio final modif

--- index.js
+++ index.js
@@ -2,33 +2,60 @@ console.log("Hey !")
 function affich() {
     document.write ('hey!')
 }
+const obj = { toto: "truc", bidule: 42 };
+function testtest() {
+    Object.entries(obj).forEach(
+    ([clé, valeur]) => console.log(` ${valeur}`)
+);}
 
-const projet = ['Projet 1', 'Projet 2', 'Projet 3', 'Projet 4', 'Projet 5', 'Projet 6']
+testtest()
 
-function updateColor() {
-     Math.floor(Math.random() * 255);
-    } ; 
+
+
+    const projet = ['Projet 1', 'Projet 2', 'Projet 3', 'Projet 4', 'Projet 5', 'Projet 6']
+
+    function affichProjectbox () { 
+        projet.forEach(function (element) {
+            var div = document.createElement("button");
+            div.className = "btn btn-outline-primary";
+            div.setAttribute("type", "button");
+            div.setAttribute("id", "projetBox");
+            div.innerHTML = element;
+            div.setAttribute("onclick", "location.href = 'projet.html'");
+            document.getElementById("section2-2").appendChild(div);
+        });}
     
-function myFunction() {
-    var btn = document.createElement("a");
-    btn.innerHTML="click";
-    btn.setAttribute("href","https://www.google.com");
-    btn.setAttribute("target","_blank");
-    document.getElementById("projetBox").appendChild(btn);
-}
+        function buttonCV () {
+        var div = document.createElement("button");
+        div.className = "btn btn-primary";
+        div.setAttribute("type", "button");
+        div.setAttribute("id", "btn btn-info");
+        document.getElementById("section1").appendChild(div);
+        var addAhref = document.createElement("a");
+        addAhref.setAttribute("href", "cv.html");
+        addAhref.setAttribute("style", "color:#FFFFFF;");
+        addAhref.innerHTML = "Voir CV";
+        document.getElementById("btn btn-info").appendChild(addAhref);
+    }
 
+        function myFunction() {
+            var btn = document.createElement("a");
+            btn.href= "projet.html"; 
+            document.getElementById("projetBox").appendChild(btn);
+        }
+
+        function testgogo() {
+            var createA = document.createElement('a');
+            var createAText = document.createTextNode(theCounter);
+            createA.setAttribute('href', "http://google.com");
+            createA.appendChild(createAText);
+            getTheTableTag.appendChild(createA);
+        }; 
 // une lambda ne permet pas d'utiliser une fonction. 
 //Utiliser des fonctions à l'intéreur des map / forEach
-function affichProjectbox () { 
-    projet.forEach(function (element) {
-        var div = document.createElement("button");
-        div.className = "btn btn-outline-primary";
-        div.innerHTML = element;
-        div.setAttribute("onClick", "myFunction()");
-        document.getElementById("section2-2").appendChild(div);
-    });
 
-function suppr() {
+
+        function suppr() {
             var div = document.createElement("p");
             document.getElementById("tata").innerHTML = "";
             document.getElementById("tata").appendChild(div);
@@ -46,6 +73,94 @@ function suppr() {
             div.innerHTML = entry; 
             document.getElementById("tata").appendChild(div);
         }    
+        function suppr1() {
+            var div = document.createElement("p");
+            document.getElementById("aproposText").innerHTML = "";
+            document.getElementById("aproposText").appendChild(div);
+        }
+        function replace1() {
+            const entry = String(window.prompt("Quelles modifications?", ""));
+            var div = document.createElement("p");
+            document.getElementById("aproposText").innerHTML = entry;
+            document.getElementById("aproposText").appendChild(div);
+        }
+        function add1() {
+            const entry = String(window.prompt("Quels ajouts?", ""));
+            var div = document.createElement("p");
+            div.innerHTML = entry; 
+            document.getElementById("aproposText").appendChild(div);
+        }
+
+        function suppr2() {
+            var div = document.createElement("p");
+            document.getElementById("exptext").innerHTML = "";
+            document.getElementById("exptext").appendChild(div);
+        }
+        function replace2() {
+            const entry = String(window.prompt("Quelles modifications?", ""));
+            var div = document.createElement("p");
+            document.getElementById("exptext").innerHTML = entry;
+            document.getElementById("exptext").appendChild(div);
+        }
+        function add2() {
+            const entry = String(window.prompt("Quels ajouts?", ""));
+            var div = document.createElement("p");
+            div.innerHTML = entry; 
+            document.getElementById("exptext").appendChild(div);
+        }
+
+        function suppr3() {
+            var div = document.createElement("p");
+            document.getElementById("comptext").innerHTML = "";
+            document.getElementById("comptext").appendChild(div);
+        }
+        function replace3() {
+            const entry = String(window.prompt("Quelles modifications?", ""));
+            var div = document.createElement("p");
+            document.getElementById("comptext").innerHTML = entry;
+            document.getElementById("comptext").appendChild(div);
+        }
+        function add3() {
+            const entry = String(window.prompt("Quels ajouts?", ""));
+            var div = document.createElement("p");
+            div.innerHTML = entry; 
+            document.getElementById("comptext").appendChild(div);
+        }
+
+        function suppr4() {
+            var div = document.createElement("p");
+            document.getElementById("formtext").innerHTML = "";
+            document.getElementById("formtext").appendChild(div);
+        }
+        function replace4() {
+            const entry = String(window.prompt("Quelles modifications?", ""));
+            var div = document.createElement("p");
+            document.getElementById("formtext").innerHTML = entry;
+            document.getElementById("formtext").appendChild(div);
+        }
+        function add4() {
+            const entry = String(window.prompt("Quels ajouts?", ""));
+            var div = document.createElement("p");
+            div.innerHTML = entry; 
+            document.getElementById("formtext").appendChild(div);
+        }
+        function suppr5() {
+            var div = document.createElement("p");
+            document.getElementById("textboxnooverflow").innerHTML = "";
+            document.getElementById("textboxnooverflow").appendChild(div);
+        }
+        function replace5() {
+            const entry = String(window.prompt("Quelles modifications?", ""));
+            var div = document.createElement("p");
+            document.getElementById("textboxnooverflow").innerHTML = entry;
+            document.getElementById("textboxnooverflow").appendChild(div);
+        }
+        function add5() {
+            const entry = String(window.prompt("Quelles modifications?", ""));
+            var div = document.createElement("p");
+            div.innerHTML = entry; 
+            document.getElementById("textboxnooverflow").appendChild(div);
+        }
 
 function affichModal() {
     // Get the modal
@@ -74,11 +189,5 @@ function affichModal() {
     }
 }    
 
-var menuButton = document.querySelector('#menu-button');
-var menu = document.querySelector('#menu');
-// show or hide
-menuButton.addEventListener('click',function(){
-  menu.classList.toggle('show-menu');
-  menuButton.classList.toggle('close');
-});
-}
+
+
